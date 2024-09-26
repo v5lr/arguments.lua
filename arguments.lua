2a231341c86a46ac3af5c3e2812d91aed1a1a3c6
cam <sorableisdad@gmail.com>
2024-09-25 21:07:58 -0500

    Add files via upload

---
+++ arguments.lua
@@ -0,0 +1,24 @@
+return {
+    ["UpdateMousePosI2"] = {
+        2788229376,  -- Da Hood
+        7213786345,  -- Da Hood VC
+        16033173781, -- Da Hood Macro
+    },
+    
+    ["UpdateMousePos"] = {
+        17897702920,  -- OGDH
+        11143225577,  -- 1v1 AimTrainer
+        17502123056,  -- AR Hood
+        14174018683,  -- JS Hood
+        18177028900,  -- Wanted Hood
+        18459606288,  -- Fatal Hood
+        17721393836,  -- The Hood Customs
+        17809101348,  -- New Hood
+        16859411452,  -- Hood Z
+        18129399112,  -- Del Hood
+        17079102898,  -- Italian Hood
+        17061444544,  -- Rae Hood
+        17344804827,  -- Yeno Hood
+        15644861772,  -- Flame Hood
+    }
+}
